d9ced9fae0b82d0893458d74faee5a984d881515
alexdneprin <alexdneprin@gmail.com>
2021-11-10 19:50:47 +0200

    Added label

--- CommonProject/CommonProject/ViewController.swift
+++ CommonProject/CommonProject/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = "Hello!"
+        
     }
-
 
 }
 
